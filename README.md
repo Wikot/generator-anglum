40d4c1ff3e61f8fb3a08f105f51af5c11b2eacea
Matías Halles <matias.halles@gmail.com>
2015-05-23 15:58:02 -0300

    Init: Basic project configurations (gitignore, editorconfig, composer, gulp, bower) for testing that i'm not completely lost here.

---
+++ README.md
@@ -0,0 +1,35 @@
+# generator-anglum
+
+> A [Yeoman](http://yeoman.io) generator
+
+## Getting Started
+
+### What is Anglum?
+
+AngularJS + Lumen Yeoman Generator.
+
+### Features
+
+* AngularJS
+* Lumen (Microframework from Laravel guys)
+* Gulp
+* Composer
+* Bower
+
+## Install
+
+```bash
+npm install -g generator-anglum
+```
+
+## Usage:
+
+```bash
+mkdir your-project-dir
+cd your-project-dir
+yo anglum
+```
+
+## License
+
+MIT
---
+++ generators/app/index.js
@@ -0,0 +1,67 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+
+module.exports = yeoman.generators.Base.extend({
+  prompting: function () {
+    var done = this.async();
+
+    this.log(yosay(
+      'Welcome to the badass ' + chalk.red.bgYellow('Anglum') + ' generator!'
+    ));
+
+    var prompts = [];
+
+    this.prompt(prompts, function (props) {
+      this.props = props;
+      // To access props later use this.props.someOption;
+
+      done();
+    }.bind(this));
+  },
+
+  writing: {
+    app: function () {
+      this.fs.copy(
+        this.templatePath('_package.json'),
+        this.destinationPath('package.json')
+      );
+      this.fs.copy(
+        this.templatePath('_bower.json'),
+        this.destinationPath('bower.json')
+      );
+      this.fs.copy(
+        this.templatePath('_composer.json'),
+        this.destinationPath('composer.json')
+      );
+      this.fs.copy(
+        this.templatePath('_gulpfile.js'),
+        this.destinationPath('gulpfile.js')
+      );
+    },
+
+    projectfiles: function () {
+      this.fs.copy(
+        this.templatePath('gitignore'),
+        this.destinationPath('.gitignore')
+      );
+      this.fs.copy(
+        this.templatePath('bowerrc'),
+        this.destinationPath('.bowerrc')
+      );
+      this.fs.copy(
+        this.templatePath('editorconfig'),
+        this.destinationPath('.editorconfig')
+      );
+      this.fs.copy(
+        this.templatePath('jshintrc'),
+        this.destinationPath('.jshintrc')
+      );
+    }
+  },
+
+  install: function () {
+    this.installDependencies();
+  }
+});
